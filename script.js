0914ff24232dbfa7112c40c6482ad0e6d0022332
Duane Christian Glorioso <130958736+Goenjii@users.noreply.github.com>
2023-04-24 14:50:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
-// get the game board element
-const gameBoard = document.getElementById('game-board');
-
-// set the score
-let score = 0;
-const scoreBoard = document.getElementById('score');
-scoreBoard.innerText = score;
-
-// function to randomly select a hole
-function randomHole() {
-  const holes = document.querySelectorAll('.hole');
-  const randomIndex = Math.floor(Math.random() * holes.length);
-  const hole = holes[randomIndex];
-  return hole;
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
 }
 
-// function to show the mole
-function showMole() {
-  const hole = randomHole();
-  const mole = hole.querySelector('.mole');
-  mole.style.display = 'block';
-
-  // hide the mole after a random time
-  const randomTime = Math.floor(Math.random() * 1000) + 500;
-  setTimeout(() => {
-    mole.style.display = 'none';
-    // recursively call the function to show the next mole
-    showMole();
-  }, randomTime);
-}
-
-// add event listener to the game board
-gameBoard.addEventListener('click', (event) => {
-  const mole = event.target;
-  if (mole.classList.contains('mole')) {
-    score++;
-    scoreBoard.innerText = score;
-    mole.style.display = 'none';
-  }
+const playButton = document.getElementById('play-btn');
+playButton.addEventListener('click', () => {
+    alert('You won!');
 });
-
-// start the game
-showMole();
